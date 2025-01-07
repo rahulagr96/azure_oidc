469072c48e7dd28bc51e6964867939e5524297b9
Rahul Agrawal <rahul.agrawal@zeiss.com>
2025-01-07 09:10:20 +0100

    added

---
+++ terraform-app/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+provider "azuread" {
+  tenant_id = "ee1faacd-8bbe-4894-b531-154f41b175ee"
+}
+
+# Retrieve the primary Azure subscription
+data "azurerm_subscription" "primary" {}
+
+resource "azurerm_resource_group" "resource_group" {
+  name     = "test-rg"
+  location = "West Europe"
+  lifecycle {
+    ignore_changes = [
+      tags
+    ]
+  }
+}
